20e9c1e7863e4beb9394c603a694ac2c041e7590
Ilija Istuk <ilija.istuk@outlook.com>
2023-04-11 23:23:15 +0200

    Making logic more typesafe

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,6 @@
-export type LetterScoreDictionary = Record<string, number>;
+export type KnownLetter = 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k' | 'l' | 'm' | 'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'w' | 'x' | 'y' | 'z';
+
+export type LetterScoreDictionary = Record<KnownLetter, number>;
 
 export type InputChangeEvent = React.ChangeEventHandler<HTMLInputElement>
 export type FormSubmitEvent = React.FormEventHandler<HTMLFormElement>;
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 import { LETTER_SCORES } from "./consts";
 import { UnknownCharacterError } from "./errors";
-import { LetterScoreDictionary } from "./types";
+import { KnownLetter, LetterScoreDictionary } from "./types";
 
 /**
  * Get Scrabble letter score for given word.
@@ -9,18 +9,24 @@ import { LetterScoreDictionary } from "./types";
  * @throws {UnknownCharacterError} in case word contains character not specified in letterScore
  * @returns Scrabble score
  */
-export function getLetterScore(word: string, letterScores: LetterScoreDictionary = LETTER_SCORES) {
-    const arrayOfLetters = word.split('');
+export function getLetterScore(word: string, letterScores = LETTER_SCORES) {
+    const arrayOfLetters = word.toLocaleLowerCase().split('');
 
     const score = arrayOfLetters.reduce((prev, curr) => {
-        const letterScore = letterScores[curr];
-
-        if (!letterScore) {
+        if (!isKnownLetter(curr)) {
             throw new UnknownCharacterError(curr);
         }
+
+        const letterScore = letterScores[curr];
 
         return prev + letterScore;
     }, 0);
 
     return score;
+}
+
+export function isKnownLetter(letter: string): letter is KnownLetter {
+    const knownLetters = Object.keys(LETTER_SCORES);
+
+    return knownLetters.includes(letter);
 }
